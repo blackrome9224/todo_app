3ebdfacc330fe3f7fa33be467515f8b534d00234
blackrome9224 <blackrome9224@gmail.com>
2020-05-10 16:21:54 +0000

    finish 2-4

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,15 @@ module TodoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout "devise"
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+      HomeController.layout "home"
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
